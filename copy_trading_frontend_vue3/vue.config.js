c3e3a883b3acdf22c0a384c8aa0551171bc682d8
EthanXUTQ <x1341699206@gmail.com>
2024-09-17 12:18:49 +0800

    springboot:增加webconfig文件用于配置开发环境运行跨域请求。

--- copy_trading_frontend_vue3/vue.config.js
+++ copy_trading_frontend_vue3/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080, // 将端口号设置为你希望的值，例如 8080 或其他端口
+  },
 })
