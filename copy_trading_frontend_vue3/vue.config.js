7418179fbf519a3ef8091e6c6e0805f779b7a074
firmisim <yetongruo@gmail.com>
2024-09-24 17:31:05 +0800

    添加了按需导入ElementPlus组件的配置，现在可以直接去elementplus组件那里复制代码直接使用了

--- copy_trading_frontend_vue3/vue.config.js
+++ copy_trading_frontend_vue3/vue.config.js
@@ -1,6 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+
+//按需导入elementPlus组件
+const AutoImport = require('unplugin-auto-import/webpack').default;
+const Components = require('unplugin-vue-components/webpack').default;
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+const ElementPlus = require('unplugin-element-plus/webpack');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  
+  //按需导入elementPlus组件
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      ElementPlus({
+        useSource: true,
+      }),
+    ],
+  },
   devServer: {
     port: 8081, // 将端口号设置为你希望的值.
   },
