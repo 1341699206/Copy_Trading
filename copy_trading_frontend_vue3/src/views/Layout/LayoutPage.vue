ecb13bb98a2345067b9b334bb800e6c86931467c
firmisim <yetongruo@gmail.com>
2024-09-25 11:36:24 +0800

    添加了吸顶导航栏，

--- copy_trading_frontend_vue3/src/views/Layout/LayoutPage.vue
+++ copy_trading_frontend_vue3/src/views/Layout/LayoutPage.vue
@@ -1,15 +1,17 @@
 <script setup>
     import LayoutHeader from './component/LayoutHeader.vue';
     import LayoutFooter from './component/LayoutFooter.vue';
+    import LayoutFixed from './component/LayoutFixed.vue';
 </script>
 
 
 <template>
     <div>
-        这里是首页
+        <layout-fixed></layout-fixed>
         <layout-header></layout-header>
         <!-- 二级路由出口 -->
         <router-view></router-view>
         <layout-footer></layout-footer>
     </div>
 </template>
+
