ef44abea3e68a39352ac2cd9e73529ca2c70341e
firmisim <yetongruo@gmail.com>
2024-09-28 21:04:30 +0800

    调整了result.

--- copy_trading_backend/src/main/java/com/xtq_ymt/copy_trading_backend/Result/Result.java
+++ copy_trading_backend/src/main/java/com/xtq_ymt/copy_trading_backend/Result/Result.java
@@ -2,7 +2,7 @@ package com.xtq_ymt.copy_trading_backend.Result;
 
 import lombok.*;
 
-// 通用返回结构的外部类
+// 标准响应结果
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,33 +12,17 @@ public class Result {
     private Object data;   // 返回的数据（可以是各种类型）
 
     // 操作成功但没有数据
-    public static Result success() {
-        return new Result(1, "success", null);
+    public static Result success(String msg) {
+        return new Result(1,msg,null);
     }
 
     // 操作成功并带有数据
-    public static Result success(Object data) {
-        return new Result(1, "success", data);
+    public static Result success(String msg,Object data) {
+        return new Result(1, msg, data);
     }
 
     // 操作失败并带有错误信息
     public static Result error(String msg) {
         return new Result(0, msg, null);
-    }
-
-    // 内部类 AuthResult 用于处理认证结果
-    @Data
-    @AllArgsConstructor
-    public static class AuthResult {
-        private boolean success;
-        private String message;
-    }
-
-    // 内部类 Country 用于表示国家信息
-    @Data
-    @AllArgsConstructor
-    public static class Country {
-        private String code;
-        private String name;
     }
 }
--- copy_trading_backend/src/main/java/com/xtq_ymt/copy_trading_backend/controller/AuthController.java
+++ copy_trading_backend/src/main/java/com/xtq_ymt/copy_trading_backend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.xtq_ymt.copy_trading_backend.controller;
 
+import com.xtq_ymt.copy_trading_backend.Result.Country;
 import com.xtq_ymt.copy_trading_backend.Result.Result;
 import com.xtq_ymt.copy_trading_backend.dto.LoginRequest;
 import com.xtq_ymt.copy_trading_backend.dto.RegisterRequest;
@@ -61,9 +62,9 @@ public class AuthController {
     // 获取国家列表
     @GetMapping("/register/countries")
     public Result getCountries() {
-        List<Result.Country> countries = new ArrayList<>();
-        countries.add(new Result.Country("CN", "China"));
-        countries.add(new Result.Country("US", "United States"));
-        return Result.success(countries);
+        List<Country> countries = new ArrayList<>();
+        countries.add(new Country("CN", "China"));
+        countries.add(new Country("US", "United States"));
+        return Result.success("success",countries);
     }
 }
