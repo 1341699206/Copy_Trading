b73604ba0862dac89b2bfdb1fccbab851e8535af
firmisim <yetongruo@gmail.com>
2024-10-01 00:50:39 +0800

    token失效处理

--- copy_trading_frontend_vue3/src/utils/http.js
+++ copy_trading_frontend_vue3/src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
 import { useUserStore } from "@/stores/user";
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL: 'http://localhost:9099',
@@ -23,11 +24,19 @@ httpInstance.interceptors.request.use(config =>{
 
 // axios响应拦截器
 httpInstance.interceptors.response.use(res=>res.data,e=>{
+    const userStore =useUserStore()
     //统一错误提示
     ElMessage({
         type:'warning',
         message: e.response.data.msg
     })
+
+    //token失效处理
+    if(e.response.status === 401){
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
+
     return Promise.reject(e);
 })
 
