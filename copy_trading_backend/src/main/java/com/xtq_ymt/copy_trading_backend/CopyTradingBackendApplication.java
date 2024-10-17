4322d8de5f5379b1d28328b2f2e436b93893cd4f
firmisim <yetongruo@gmail.com>
2024-10-17 15:40:21 +0800

    重新检测了错误，更正了错误，现在应该都没问题了。

--- copy_trading_backend/src/main/java/com/xtq_ymt/copy_trading_backend/CopyTradingBackendApplication.java
+++ copy_trading_backend/src/main/java/com/xtq_ymt/copy_trading_backend/CopyTradingBackendApplication.java
@@ -2,8 +2,10 @@ package com.xtq_ymt.copy_trading_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 启用定时任务
 public class CopyTradingBackendApplication {
 
 	public static void main(String[] args) {
